a211ced5a9eff3fdc943eb2bbdb14d700930960e
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-06-25 14:32:36 +0900

    update build script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+This repository is under developement.   
 This is an experimental repository to use [Pinocchio](https://github.com/stack-of-tasks/pinocchio) from rust lang.
--- build.rs
+++ build.rs
@@ -1,19 +1,22 @@
 extern crate pkg_config;
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/main.rs");
-    println!("cargo:rerun-if-changed=src/multibody/model.rs");
-    println!("cargo:rerun-if-changed=src/multibody/data.rs");
-
-    let pinocchio= pkg_config::Config::new().atleast_version("2.6.4").statik(false).probe("pinocchio").unwrap();
-    println!("{:?}", pinocchio);
     println!("cargo:rustc-link-lib=dylib=stdc++");
+    let pinocchio= pkg_config::Config::new().atleast_version("2.6.4").statik(false).probe("pinocchio").unwrap();
     for lib in pinocchio.libs {
         println!("cargo:rustc-link-lib=dylib={}", lib);
     } 
     for path in pinocchio.link_paths {
         println!("cargo:rustc-link-search=all={}", path.as_os_str().to_str().unwrap());
     } 
+
+    // For debugging
+    // println!("=====================================================");
+    // println!("=====================================================");
+    // println!("{:?}", pinocchio);
+    // println!("=====================================================");
+    // println!("=====================================================");
+
     cxx_build::bridge("src/multibody/model.rs")
         .file("src/cpp/src/multibody/model.cpp")
         .include("/usr/include/eigen3")
@@ -23,8 +26,6 @@ fn main() {
         .flag_if_supported("-O3")
         .flag_if_supported("-NDEBUG")
         .flag_if_supported("-march=native")
-        .flag_if_supported("-L /usr/lib/x86_64-linux-gnu")
-        .flag_if_supported("-L /opt/openrobots/lib")
         .compile("model");
     cxx_build::bridge("src/multibody/data.rs")
         .file("src/cpp/src/multibody/data.cpp")
@@ -36,4 +37,8 @@ fn main() {
         .flag_if_supported("-NDEBUG")
         .flag_if_supported("-march=native")
         .compile("data");
+
+    println!("cargo:rerun-if-changed=src/main.rs");
+    println!("cargo:rerun-if-changed=src/multibody/model.rs");
+    println!("cargo:rerun-if-changed=src/multibody/data.rs");
 }
