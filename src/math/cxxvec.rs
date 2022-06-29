337ff98b2de13e821459cf67c13710c2000986ab
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-06-29 18:35:00 +0900

    add conversion in cxxvec

--- src/math/cxxvec.rs
+++ src/math/cxxvec.rs
@@ -1,5 +1,5 @@
 use cxx::{UniquePtr, CxxVector};
-use nalgebra::{Vector3, Matrix3};
+use nalgebra::{Vector3, Matrix3, DVector, DMatrix};
 
 
 pub fn cxxvec_to_vector3(cxx_vec: &UniquePtr<CxxVector<f64>>) -> Option<Vector3<f64>> {
@@ -24,6 +24,38 @@ pub unsafe fn cxxvec_to_vector3_unchecked(cxx_vec: &UniquePtr<CxxVector<f64>>) -
         let v1 = cxx_vec.get_unchecked(1);
         let v2 = cxx_vec.get_unchecked(2);
         let v = Vector3::new(*v0, *v1, *v2);
+        Some(v)
+    }
+    else {
+        None
+    }
+}
+
+
+pub fn cxxvec_to_dvector(cxx_vec: &UniquePtr<CxxVector<f64>>, size: u32) -> Option<DVector<f64>> {
+    let size = size as usize;
+    let len = cxx_vec.len();
+    if len == size {
+        let mut v = DVector::<f64>::zeros(size);
+        for i in 0..size {
+            v[i] = *cxx_vec.get(i).unwrap();
+        }
+        Some(v)
+    }
+    else {
+        None
+    }
+}
+
+
+pub unsafe fn cxxvec_to_dvector_unchecked(cxx_vec: &UniquePtr<CxxVector<f64>>, size: u32) -> Option<DVector<f64>> {
+    let size = size as usize;
+    let len = cxx_vec.len();
+    if len == size {
+        let mut v = DVector::<f64>::zeros(size);
+        for i in 0..size {
+            v[i] = *cxx_vec.get_unchecked(i);
+        }
         Some(v)
     }
     else {
@@ -70,6 +102,44 @@ pub unsafe fn cxxvec_to_matrix3_unchecked(cxx_vec: &UniquePtr<CxxVector<f64>>) -
         let m = Matrix3::new(*v0, *v1, *v2, 
                                                                  *v3, *v4, *v5,
                                                                  *v6, *v7, *v8);
+        Some(m)
+    }
+    else {
+        None
+    }
+}
+
+
+pub fn cxxvec_to_dmatrix(cxx_vec: &UniquePtr<CxxVector<f64>>, rows: u32, cols: u32) -> Option<DMatrix<f64>> {
+    let rows = rows as usize;
+    let cols = cols as usize;
+    let len = cxx_vec.len();
+    if len == (rows * cols) {
+        let mut m = DMatrix::<f64>::zeros(rows, cols);
+        for i in 0..rows {
+            for j in 0..cols {
+                m[(i, j)] = *cxx_vec.get(i*rows+j).unwrap();
+            }
+        }
+        Some(m)
+    }
+    else {
+        None
+    }
+}
+
+
+pub unsafe fn cxxvec_to_dmatrix_unchecked(cxx_vec: &UniquePtr<CxxVector<f64>>, rows: u32, cols: u32) -> Option<DMatrix<f64>> {
+    let rows = rows as usize;
+    let cols = cols as usize;
+    let len = cxx_vec.len();
+    if len == (rows * cols) {
+        let mut m = DMatrix::<f64>::zeros(rows, cols);
+        for i in 0..rows {
+            for j in 0..cols {
+                m[(i, j)] = *cxx_vec.get_unchecked(i*rows+j);
+            }
+        }
         Some(m)
     }
     else {
